722505e43a34b53263cb2d6efb240baa5c4a3513
leeyuchang <leeyucha@gmail.com>
2021-06-18 09:17:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect, useCallback } from 'react'
+// import { debounce } from 'lodash'
+import './App.scss'
 
-function App() {
+export default function App() {
+  const bottomSliderHeightRef = useRef(0)
+  const [visible, setVisible] = useState(false)
+
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  const handleResize = useCallback(() => {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight })
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [handleResize])
+
+  const handleClick = useCallback(() => {
+    if (visible) {
+      bottomSliderHeightRef.current = 10
+    } else {
+      bottomSliderHeightRef.current = 0
+    }
+    setVisible((prev) => !prev)
+  }, [visible])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: windowSize.height }}>
+      <div
+        onClick={handleClick}
+        className="content"
+        style={{
+          height: `calc(100% - ${bottomSliderHeightRef.current}rem)`,
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt amet
+        excepturi saepe harum corporis fuga inventore voluptatibus quia
+        repudiandae repellendus. Laboriosam quos ex tenetur voluptates itaque
+        aspernatur voluptatem ut, quaerat non provident aut eos consequuntur
+        explicabo, soluta temporibus? Harum reprehenderit quisquam natus
+        perferendis id temporibus explicabo autem saepe ex nam praesentium,
+        itaque commodi, modi consectetur animi repellendus ad! Dolore unde
+        aspernatur voluptatem ut, quaerat non provident aut eos consequuntur
+        explicabo, soluta temporibus? Harum reprehenderit quisquam natus
+        perferendis id temporibus explicabo autem saepe ex nam praesentium,
+        itaque commodi, modi consectetur animi repellendus ad! Dolore unde
+        aspernatur voluptatem ut, quaerat non provident aut eos consequuntur
+        explicabo, soluta temporibus? Harum reprehenderit quisquam natus
+        perferendis id temporibus explicabo autem saepe ex nam praesentium,
+        itaque commodi, modi consectetur animi repellendus ad! Dolore unde
+        aspernatur voluptatem ut, quaerat non provident aut eos consequuntur
+        explicabo, soluta temporibus? Harum reprehenderit quisquam natus
+        perferendis id temporibus explicabo autem saepe ex nam praesentium,
+        itaque commodi, modi consectetur animi repellendus ad! Dolore unde
+        officiis saepe magni eos, non recusandae alias dolores, minus earum
+        neque, suscipit quam ut ipsa corrupti ipsam quas aut veniam ullam. Culpa
+        debitis ullam totam aperiam rerum.
+        <p>{window.innerHeight}</p>
+      </div>
+      <div
+        onClick={handleClick}
+        className="bottomSlider"
+        style={{
+          height: `${bottomSliderHeightRef.current}rem`,
+          opacity: bottomSliderHeightRef.current ? 1 : 0,
+        }}
+      ></div>
     </div>
-  );
+  )
 }
-
-export default App;
